9f68da8ce1307375af4d0d27f5c039d9d266d3f4
Justin Alex Paramanandan <justin.alex@servicerocket.com>
2018-01-11 11:03:09 +1100

    Updated code to 7.x supported API

--- jira-server/workflows/post-functions/CreateSelectListOptionsFromCascadingSelect.groovy
+++ jira-server/workflows/post-functions/CreateSelectListOptionsFromCascadingSelect.groovy
@@ -1,101 +1,69 @@
 /**
-  * @author justin.alex
-  * @version 1.0
+  * Name: ValidateTwoDatesWorkflow.groovy
+  * @author jalex
+  * @version 2.1
   *
-  * A Groovy script to copy the parent and child of the Category cascading select field into
-  * the parent category and child category select lists. These fields can then be used
-  * for reporting.
+  * A Groovy script to compare the Due Date field against the Resolved Date field. If the Resolved Date is after the
+  * Due Date, then the Target Met field would be set to "No". Otherwise, it would be set to "Yes"
   *
   */
 
 import com.atlassian.jira.component.ComponentAccessor
-import com.atlassian.jira.issue.MutableIssue
+import com.atlassian.jira.issue.CustomFieldManager
 import com.atlassian.jira.issue.ModifiedValue
-import com.atlassian.jira.issue.customfields.impl.CascadingSelectCFType
 import com.atlassian.jira.issue.customfields.manager.OptionsManager
-import com.atlassian.jira.issue.customfields.option.Options
 import com.atlassian.jira.issue.customfields.option.Option
 import com.atlassian.jira.issue.fields.CustomField
-import com.atlassian.jira.issue.fields.config.FieldConfigScheme
-import com.atlassian.jira.issue.fields.config.FieldConfig
+import com.atlassian.jira.issue.fields.layout.field.FieldLayoutItem
 import com.atlassian.jira.issue.util.DefaultIssueChangeHolder
+import java.text.SimpleDateFormat
 
-// USER DEFINED VARIABLES
-def categoryFieldName = "Category"
-def categoryFieldID = "customfield_11706"
-def parentCategoryFieldID = "customfield_12303"
-def childCategoryFieldID = "customfield_12304"
+// Constants declarations
+def targetMetFieldName = "Target Met"
+Long targetMetYesOptionID = 10001
+Long targetMetNoOptionID = 10002
 
-// INIT FIELD OBJECTS
-def customFieldManager = ComponentAccessor.customFieldManager
-def userCategoryField = customFieldManager.getCustomFieldObject(categoryFieldID)
-def parentCategoryField = customFieldManager.getCustomFieldObject(parentCategoryFieldID)
-def childCategoryField = customFieldManager.getCustomFieldObject(childCategoryFieldID)
+// We grab the related fields here
+CustomFieldManager customFieldManager = ComponentAccessor.CustomFieldManager
+CustomField targetMetField = customFieldManager.getCustomFieldObjectByName(targetMetFieldName)
+String dueDateValue = issue.getDueDate()
+String resolvedDateValue = issue.getResolutionDate()
 
-MutableIssue issue = (MutableIssue) event.issue
-Map<String, Option> params = (HashMap<String,Option>) issue.getCustomFieldValue(userCategoryField)
-
-if (params != null) {
-    Option parent = params.get(CascadingSelectCFType.PARENT_KEY)
-    Option child = params.get(CascadingSelectCFType.CHILD_KEY)
-    
-    // add options to the select list
-    Option parentOption = addOptionToCustomField(parentCategoryField, parent.getValue())
-    Option childOption = addOptionToCustomField(childCategoryField, child.getValue())
-
-    // Update parent and child select lists
-    if (parentOption != null && childOption != null) {
-        parentCategoryField.updateValue(null, issue , new ModifiedValue(issue.getCustomFieldValue(parentCategoryField), parentOption), new DefaultIssueChangeHolder());
-        childCategoryField.updateValue(null, issue , new ModifiedValue(issue.getCustomFieldValue(childCategoryField), childOption), new DefaultIssueChangeHolder());
-    }
+// Some error checking
+if (dueDateValue.equals(null)) {
+    log.info "No due date specified for issue ${issue.getKey()}"
+} else if (resolvedDateValue.equals(null)) {
+    log.info "No resolved date specified for issue ${issue.getKey()}"
+} else if (targetMetField.equals(null)) {
+    log.info "Custom field ${targetMetFieldName} does not exist in the instance."
 }
 
-// Method to add new option to select list
-public Option addOptionToCustomField(CustomField customField, String value) {
-    Option newOption = null
-    if (customField != null) {
-        List<FieldConfigScheme> schemes = customField.getConfigurationSchemes()
-        if (schemes != null && !schemes.isEmpty()) {
-            FieldConfigScheme sc = schemes.get(0)
-            Map configs = sc.getConfigsByConfig()
-            if (configs != null && !configs.isEmpty()) {
-                FieldConfig config = (FieldConfig) configs.keySet().iterator().next()
-                OptionsManager optionsManager = ComponentAccessor.optionsManager
-                Options options = optionsManager.getOptions(config)
+// Parsing the Resolved Date and Due Date fields as a Date() object
+Date dueDate = new SimpleDateFormat("yyyy-MM-dd").parse(dueDateValue)
+Date resolveDate = new SimpleDateFormat("yyyy-MM-dd").parse(resolvedDateValue)
 
-                // check if passed option already exists
-                if (!newValueAlreadyPresent(options, value)) {
+log.info "Due Date for ${issue.getKey()} is ${dueDate.toString()}"
+log.info "Resolved Date for ${issue.getKey()} is ${resolveDate.toString()}"
 
-                    // if passed option doesn't exist, add them to the select list
-                    int nextSequence = options.isEmpty() ? 1 : options.getRootOptions().size() + 1
-                    newOption = optionsManager.createOption(config, null, (long) nextSequence, value)
-                    log.warn "Option ${value} added to ${customField.getFieldName()}."
-                } else {
+// We grab the available options from the custom field
+Object selectValue = issue.getCustomFieldValue(targetMetField) //grab current value for the custom field
+FieldLayoutItem fieldLayoutItem = ComponentAccessor.getFieldLayoutManager().getFieldLayout(issue).getFieldLayoutItem(targetMetField)
+OptionsManager optionsManager = (OptionsManager) ComponentAccessor.getComponentOfType(OptionsManager.class)
+Option targetMetNoOption = optionsManager.findByOptionId(targetMetNoOptionID)
+Option targetMetYesOption = optionsManager.findByOptionId(targetMetYesOptionID)
 
-                    // passed option exists, get existing option
-                    newOption = getCurrentOption(options, value)
-                    log.warn "Option ${value} already exist."
-                }
-            }
-        }
-    }
-
-    return newOption;
+if (targetMetNoOption.equals(null)) {
+    log.info "There is no option with the ID ${targetMetNoOptionID}"
+} else if (targetMetYesOption.equals(null)) {
+    log.info "There is no option with the ID ${targetMetYesOptionID}"
 }
 
-// method to check existence of value in select list
-private boolean newValueAlreadyPresent(List<Option> rootOptions, String value) {
-    for (Option o : rootOptions) {
-        if (o.getValue().equalsIgnoreCase(value))
-            return true
-    } 
-    return false
-}
+// Date comparisons go here
+if (resolveDate.after(dueDate)) {
+    targetMetField.updateValue(fieldLayoutItem, issue, new ModifiedValue(selectValue, targetMetNoOption), new DefaultIssueChangeHolder())
+    log.info "Custom field ${targetMetField.getFieldName()} for issue ${issue.getKey()} is set to No"
 
-// method to return current option from select list
-private Option getCurrentOption (List<Option> rootOptions, String value) {
-    for (Option o : rootOptions) {
-        if (o.getValue().equals(value))
-            return o;
-    }
+} else {
+    targetMetField.updateValue(fieldLayoutItem, issue, new ModifiedValue(selectValue, targetMetYesOption), new DefaultIssueChangeHolder())
+    log.info "Custom field ${targetMetField.getFieldName()} for issue ${issue.getKey()} is set to Yes"
 }
