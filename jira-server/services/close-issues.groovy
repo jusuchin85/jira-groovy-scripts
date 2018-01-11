11b0a5b066594f761a71bebd84e118f33d1cc7b9
Justin Alex Paramanandan <justin.alex@servicerocket.com>
2018-01-11 11:06:48 +1100

    Auto stash before revert of "Updated code to 7.x supported API"

--- jira-server/services/close-issues.groovy
+++ jira-server/services/close-issues.groovy
@@ -1,7 +1,7 @@
 /**
  * Name: close-issues.groovy
  * @author justin.alex
- * @version 2.1
+ * @version 2.0
  *
  * A Groovy script to automatically close issues in Done status after 5 days
  */
@@ -17,10 +17,12 @@ import com.atlassian.jira.component.ComponentAccessor
 import com.atlassian.jira.user.ApplicationUser
 import com.atlassian.jira.issue.search.SearchProvider
 import com.atlassian.query.Query
+import org.apache.log4j.Level
+import org.apache.log4j.Logger
 
-//log = Logger.getLogger("com.onresolve.jira.groovy.TransitionWorkflow");
-//log.setLevel(Level.INFO); // to debug script, update Level to Level.DEBUG
-log.info "Starting process to close old done issues"
+log = Logger.getLogger("com.onresolve.jira.groovy.TransitionWorkflow");
+log.setLevel(Level.INFO); // to debug script, update Level to Level.DEBUG
+log.info("Starting process to close old done issues");
 
 // Global variables to be updated before utilising the script
 int daysSinceResolved = 5 ; // days to check for issue closure
@@ -35,7 +37,7 @@ JqlClauseBuilder builder = JqlQueryBuilder.newClauseBuilder();
 builder = builder.project().in(project).and().status(previousStatus);
 builder = builder.and().not().resolutionDateAfter("-${daysSinceResolved}d");
 Query query = builder.buildQuery();
-log.debug "Query: ${query}"
+log.debug("Query: ${query}");
 
 ApplicationUser user = ComponentAccessor.UserManager.getUserByName(username);
 PagerFilter filter = PagerFilter.getUnlimitedFilter();
@@ -43,32 +45,30 @@ SearchProvider searchProvider = ComponentAccessor.getComponent(SearchProvider.cl
 Collection issues = searchProvider.search(query,user,filter).getIssues();
 
 if (issues.size() == 0)
-    log.info "No issues in ${previousStatus} status for the past ${daysSinceResolved} days to close."
+    log.info("No issues in ${previousStatus} status for the past ${daysSinceResolved} days to close.");
 else {
-    log.info "Found ${issues.size()} issue(s) in ${previousStatus} status for the past ${daysSinceResolved} days in need of closure"
+    log.info("Found ${issues.size()} issue(s) in ${previousStatus} status for the past ${daysSinceResolved} days in need of closure");
     ComponentAccessor.getJiraAuthenticationContext().setLoggedInUser(user);
 
     for (Issue issue in issues) {
-        log.info "Closing ${issue.key}: ${issue.summary}"
+        log.info("Closing ${issue.key}: ${issue.summary}");
         IssueInputParameters params = ComponentAccessor.getIssueService().newIssueInputParameters();
         params.setRetainExistingValuesWhenParameterNotProvided(true);
-        if (comment) params.setComment(comment)
-            log.debug "Action parameters: ${params.actionParameters}"
+        if (comment) params.setComment(comment);
+        log.debug("Action parameters: ${params.actionParameters}");
 
         TransitionValidationResult validation = ComponentAccessor.getIssueService().validateTransition(user,issue.getId(),actionId,params);
-        log.debug "Addition inputs: ${validation.additionInputs}"
+        log.debug("Addition inputs: ${validation.additionInputs}");
         IssueResult result;
         if (validation.isValid()) {
             result = ComponentAccessor.getIssueService().transition(user,validation);
-            if (result.isValid())
-                log.debug "${result.errorCollection}"
-            else
-                log.error "${result.errorCollection}"
+            if (result.isValid()) log.debug("${result.errorCollection}");
+            else log.error("${result.errorCollection}");
         } else {
             result = null;
-            log.error "Errors: ${validation.errorCollection.errors}"
+            log.error("Errors: ${validation.errorCollection.errors}");
         }
     }
 
-    log.info "Done closing ${issues.size()} issue(s)"
+    log.info("Done closing ${issues.size()} issue(s)");
 }
